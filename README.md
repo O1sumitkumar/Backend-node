d594f7250fdb755f2dc8360a4b1b8e9f87b7df97
sumit kumar jha <o1sumeetkumar@gmail.com>
2023-11-12 20:28:26 +0530

    CRUD in mongodb

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 ![query string](image-1.png)
 ![types of middleware](image-2.png)
 // middleware only use with routes.
+
+// MongoDB CRUD Command
+
+
+// To get MongoDB list ==> show dbs
+
+// if you write use dbName then you will be switch to that database if exist , if it not then new DB will be created.
+
+// ? if created db does not have any collection then that db doesn't show in GUI list as well as in CLI .
+
+// * To Create DB ==> use collectionName
+
+// * To Create collection in DB ==> db.createCollection( 'collectionName' ) 
+
+// * To delete dataBase ==> db.dropDatabase( 'dataBaseName' )
+
+// * To Show all the collection ==> show collections
+
+// * To delete DB ==> db.dropDatabase()
+
+// * To get current db ==> just type db and enter
+
+// first got to the collection then ==> db.user.insertOne({ name:"", data:"" })
+
+// This is to read ==> db.user.findOne() ==> This will return all the collection data.
+
+// This will find specific data ==> db.collectionName.findOne({ keyToBeSearch:"propertyToBeSearch" })
+
+// This is to update ==> db.user.updateOne({ brand:"apple" },{$set:{ fieldName:"updatedValue" }})
+
+// This will delete data ==> db.user.deleteOne({ keyToiBeDelete:"propertyToBeDelete" })
--- middleware.js
+++ middleware.js
@@ -2,12 +2,15 @@ const express = require("express");
 const isEmailProvided = require("./middleware/middleware");
 const app = express();
 
-// this is to avoid multiple  writing one by on in routes e.g app.get('/', filterFun, () => )
+//? this is to avoid multiple  writing one by one in routes e.g app.get('/', filterFun, () => )
 const route = express.Router();
 
 app.listen(5000, () => {
   console.log("Your api is running on 5000 port 😒");
 });
+
+//? Here we use our middleware 'NOTE' ==> this will applicable on across application level.
+// app.use(reqFilter);
 
 // this will work as filter middleware ( this is example of application level middleware)
 const reqFilter = (req, res, next) => {
@@ -20,21 +23,8 @@ const reqFilter = (req, res, next) => {
   }
 };
 
-//? Here we use our middleware this will applicable on application label
-// app.use(reqFilter);
 
-// here also need to pass your middle ware to route so this will effect
-route.use(isEmailProvided);
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/home", (req, res) => {
-  res.send("This is home page");
-});
-
-//? if you want to add middleware in a specific route then
+//? This is middleware which handles specific filtration in request and response and next will allow to move ahead otherwise this will keep loading at same page.
 const ifEmailProvided = (req, res, next) => {
   if (!req.query.email) {
     res.send("Please enter email");
@@ -43,6 +33,20 @@ const ifEmailProvided = (req, res, next) => {
   }
 };
 
+//? here also need to pass your middleware to route so this will use your middleware function.
+route.use(isEmailProvided);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// ? this route is using middleware individually at route level.
+app.get("/home", isEmailProvided, (req, res) => {
+  res.send("This is home page");
+});
+
+
+// ? Which ever routes has 'route.get()' will have middleware effect on that
 route.get("/about", (req, res) => {
   res.send("This is about page");
 });
@@ -55,4 +59,5 @@ route.get("/help", (req, res) => {
   res.send("This is help page");
 });
 
+// ? You have to pass route to 'app.use("/",middlewareName)'
 app.use("/", route);
